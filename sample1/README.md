99a704f81004b83393df28e4d06e7efbd255c474
unai <unai.mayo@es.ibm.com>
2018-05-03 15:19:26 +0000

    First commit: singe VM with public and private IPs

---
+++ sample1/README.md
@@ -0,0 +1,8 @@
+## sample1 infrastructure
+
+Created 5/3/18 by ucdpadmin. for sample1
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ sample1/variables.tf
@@ -0,0 +1,51 @@
+#####################################################################
+##
+##      Created 5/3/18 by ucdpadmin. for sample1
+##
+#####################################################################
+
+variable "ibm_bmx_api_key" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ibm_sl_username" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ibm_sl_api_key" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vm1_domain" {
+  type = "string"
+  description = "The domain for the computing instance."
+}
+
+variable "vm1_hostname" {
+  type = "string"
+  description = "The hostname for the computing instance."
+}
+
+variable "vm1_datacenter" {
+  type = "string"
+  description = "The datacenter in which you want to provision the instance. NOTE: If dedicated_host_name or dedicated_host_id is provided then the datacenter should be same as the dedicated host datacenter."
+}
+
+variable "vm1_os_reference_code" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ibm_network_public_vlan_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ibm_network_private_vlan_id" {
+  type = "string"
+  description = "Generated"
+}
+
