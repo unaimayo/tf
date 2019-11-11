f65c16aadc4faf2839197e22c09b3e32039b5a51
unai <unai.mayo@es.ibm.com>
2019-11-11 22:01:18 +0000

    first commit

--- tomcat/IBM/main.tf
+++ tomcat/IBM/main.tf
@@ -70,12 +70,19 @@ resource "null_resource" "execute_ansible" {
   # Create the Host File for example
   provisioner "file" {
     content = <<EOF
-default ansible_host=${ibm_compute_vm_instance.ubuntu_small_virtual_guest.ipv4_address} ansible_user='root' ansible_ssh_private_key_file='${tls_private_key.ssh.private_key_pem}' ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+default ansible_host=${ibm_compute_vm_instance.ubuntu_small_virtual_guest.ipv4_address} ansible_user='root' ansible_ssh_private_key_file=ssh_key ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
 
     destination = "/tmp/ansible-playbook-host"
   }
 
+  provisioner "file" {
+    content = <<EOF
+${tls_private_key.ssh.private_key_pem}
+EOF
+
+    destination = "/tmp/ssh_key"
+  }
   # Execute the script remotely
   provisioner "remote-exec" {
     inline = [
