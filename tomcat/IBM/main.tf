9d280091f153d4080101ec66be5c574ad51bacc8
unai <unai.mayo@es.ibm.com>
2019-11-11 21:42:33 +0000

    first commit

--- tomcat/IBM/main.tf
+++ tomcat/IBM/main.tf
@@ -70,13 +70,7 @@ resource "null_resource" "execute_ansible" {
   # Create the Host File for example
   provisioner "file" {
     content = <<EOF
-default ansible_host=${ibm_compute_vm_instance.ubuntu_small_virtual_guest.ipv4_address}"input_datatypes": [  {
-    "name": "ansible",
-    "label": "Ansible Data"
-  } ],"input_datatypes": [  {
-    "name": "ansible",
-    "label": "Ansible Data"
-  } ], ansible_user='root' ansible_ssh_private_key_file='${tls_private_key.ssh.private_key_pem}' ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+default ansible_host=${ibm_compute_vm_instance.ubuntu_small_virtual_guest.ipv4_address} ansible_user='root' ansible_ssh_private_key_file='${tls_private_key.ssh.private_key_pem}' ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
 
     destination = "/tmp/ansible-playbook-host"
